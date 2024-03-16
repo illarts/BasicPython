2ef75cee7cfbdf020827b5d94b54bff5f5dc3699
illarts <sorakuramochi@email.com>
2024-03-16 17:21:01 +0900

    euclid.py位の難易度が丁度いいかもしれない

--- euclid.py
+++ euclid.py
@@ -2,3 +2,19 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+
+a = int(a)
+b = int(b)
+
+def euclid(a, b):
+    if a < b:
+        a, b = b, a
+    
+    remain = a % b
+    
+    if remain == 0:
+        return b
+    else:
+        return euclid(b, remain)
+
+print(euclid(a, b))
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,25 @@
 from math import sin
+import math
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+
+a = 0
+b = math.pi / 2
+N = 100
+h = (b - a) / N
+k = 1
+S = 0
+
+while k <= N:
+
+    # 合計を算出
+    S += h / 2 * (sin(a + (k - 1) * h) + sin(a + k * h))
+
+    # インクリメント処理
+    k += 1
+
+print(S)
+
+# https://anotools.com/python/1116/
